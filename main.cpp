16390c3d34ab89abcee54b656718719c85822bf4
Andrés Aldana <andres.aldana.martinez@gmail.com>
2017-06-01 01:32:24 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+ struct top_table {
+    vector<string> names;
+    vector<long> scores;
+};
+
+void parse(ifstream &myfile, vector<top_table> &cases)
+{
+    string line;
+    smatch score_table_matches;
+
+    if (getline(myfile, line))
+    {
+        if (regex_match(line, regex("^\\d{1}$")))
+        {
+            cases.reserve(stoi(line));
+        }
+
+        if (regex_search(line, score_table_matches, regex("^(\\d{1})\\s(\\d{1})$")) && cases.size() < cases.capacity())
+        {
+            cases.push_back(top_table());
+            top_table *current_case_p = &cases.back();
+            current_case_p->names.reserve(stoi(score_table_matches[2]));
+            current_case_p->scores.reserve(stoi(score_table_matches[2]));
+        }
+
+
+        if (regex_search(line, score_table_matches, regex("^(\\w{3})\\s(\\d+)$")))
+        {
+            top_table *current_case = &cases.back();
+            //cout << (current_case->names.size() < current_case->names.capacity()) << endl;
+            if (current_case->names.size() < current_case->names.capacity()) {
+                current_case->names.push_back(score_table_matches[1]);
+                current_case->scores.push_back(stol(score_table_matches[2]));
+            }
+        }
+        parse(myfile, cases);
+    }
+}
+
+int main()
+{
+    vector<top_table> cases;
+    ifstream myfile("input.in");
+    if (myfile.is_open())
+    {
+        parse(myfile, cases);
+        myfile.close();
+        cout << "Number of cases " << cases.capacity() << endl;
+        cout << "Case 5 name 0 " << cases[2].names.size() << endl;
+        //cout << "Case 5 score 0 " << cases[0].scores.capacity() << endl;
+        //cout << "Error Case 6 name 0 " << cases[6].names[0] << endl;
+    }
+    return 0;
+}
