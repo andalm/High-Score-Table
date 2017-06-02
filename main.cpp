f9c064ab2bd1a5ab1d7889c59fbf25542e655f5a
Andrés Aldana <andres.aldana.martinez@gmail.com>
2017-06-02 00:32:38 -0500

    Minor changes

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
  struct top_table {
+    vector<int> position;
     vector<string> names;
     vector<long> scores;
 };
@@ -15,6 +16,7 @@ void parse(ifstream &myfile, vector<top_table> &cases)
 {
     string line;
     smatch score_table_matches;
+    top_table *current_case;
 
     if (getline(myfile, line))
     {
@@ -26,17 +28,17 @@ void parse(ifstream &myfile, vector<top_table> &cases)
         if (regex_search(line, score_table_matches, regex("^(\\d{1})\\s(\\d{1})$")) && cases.size() < cases.capacity())
         {
             cases.push_back(top_table());
-            top_table *current_case_p = &cases.back();
-            current_case_p->names.reserve(stoi(score_table_matches[2]));
-            current_case_p->scores.reserve(stoi(score_table_matches[2]));
+            current_case = &cases.back();
+            current_case->names.reserve(stoi(score_table_matches[2]));
+            current_case->scores.reserve(stoi(score_table_matches[2]));
         }
 
 
         if (regex_search(line, score_table_matches, regex("^(\\w{3})\\s(\\d+)$")))
         {
-            top_table *current_case = &cases.back();
-            //cout << (current_case->names.size() < current_case->names.capacity()) << endl;
-            if (current_case->names.size() < current_case->names.capacity()) {
+            current_case = &cases.back();
+            if (current_case->names.size() < current_case->names.capacity())
+            {
                 current_case->names.push_back(score_table_matches[1]);
                 current_case->scores.push_back(stol(score_table_matches[2]));
             }
@@ -54,7 +56,7 @@ int main()
         parse(myfile, cases);
         myfile.close();
         cout << "Number of cases " << cases.capacity() << endl;
-        cout << "Case 5 name 0 " << cases[2].names.size() << endl;
+        cout << "Case 5 name 0 " << cases[1].names.size() << endl;
         //cout << "Case 5 score 0 " << cases[0].scores.capacity() << endl;
         //cout << "Error Case 6 name 0 " << cases[6].names[0] << endl;
     }
